2b115bf0db98dbfccf7b4aba5b0c91d11544cef8
Omar Fuenmayor <omarfuenmayorroo@gmail.com>
2022-02-11 16:44:36 -0300

    gateway working

--- gateway/gateway.ts
+++ gateway/gateway.ts
@@ -1,6 +1,9 @@
 import {ApolloGateway} from '@apollo/gateway';
+import { readFileSync } from 'fs';
+import { Path } from 'typescript';
 
-export const supergraphSdl = '';
+const dirname: string = __dirname + '/supergraph.graphql';
+export const supergraphSdl = readFileSync(dirname).toString() ;
 
 export const gateway = new ApolloGateway({
   supergraphSdl
--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import {server} from './server/server';
 
 try{
   server.listen(4010).then(({url})=>{
-    console.log('running');
+    console.log(`running on ${url}`);
   });
 }catch(err){
 
